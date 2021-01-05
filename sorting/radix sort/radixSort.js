fd82a74cd2c4fc720431c0d4d75bed1bcb15eb22
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-01-05 22:50:09 +0000

    cleanup

--- sorting/radix sort/radixSort.js
+++ sorting/radix sort/radixSort.js
@@ -3,31 +3,11 @@ const { digitCount, getDigit, mostDigits } = require("./radixHelper");
 const radixSort = (arr) => {
     let mostNumbersOfDigits = mostDigits(arr);
     for (let i = 0; i < mostNumbersOfDigits; i++) {
-        let bucket = [[], [], [], [], [], [], [], [], [], []];
+        // let bucket = [[], [], [], [], [], [], [], [], [], []];
+        let bucket = Array.from({ length: 10 }, () => []); //Result in ^
         for (number of arr) bucket[getDigit(number, i)].push(number);
         arr = [];
         for (subBucket of bucket) arr = [...arr, ...subBucket];
     }
     return arr;
 };
-
-arr = [
-    12,
-    244,
-    12,
-    687,
-    123,
-    4365478,
-    2312,
-    2354,
-    56867,
-    234,
-    1234,
-    654,
-    23423,
-    575,
-    2342,
-    123,
-];
-
-console.log(radixSort(arr));
